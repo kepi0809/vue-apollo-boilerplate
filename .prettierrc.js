8d1efa99938858775ab3f53c04bb472b598edecc
Sándor Képiró <sandor.kepiro@phmu.de>
2019-08-17 14:16:51 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  // htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+}
---
+++ src/components/base/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button :class="$style.button" v-on="$listeners">
+    <slot />
+  </button>
+</template>
